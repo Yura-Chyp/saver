7e8ee4f6151a20cb1187a5f5a7c83a4f1f4b653c
Yura_Chyp <128926188+Yura-Chyp@users.noreply.github.com>
2023-08-30 11:00:38 +0300

    add regsiter and heshing password

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const { MongoClient } = require("mongodb");
 const mongoose = require('mongoose');
 const path = require('path');
 const bodyParser = require('body-parser');
@@ -7,11 +6,11 @@ const axios = require('axios');
 require('dotenv').config();
 const bcrypt = require('bcrypt');
 
-
 const app = express();
 const PORT = 3000;
 
-mongoose.connect(`mongodb+srv://admun:${process.env.DBPassword}@cluster0.vo8hxvk.mongodb.net/?retryWrites=true&w=majority`, {
+// Connect to MongoDB
+mongoose.connect(`mongodb+srv://rude22:${process.env.DBPassword}@cluster0.pxa4wp9.mongodb.net/?retryWrites=true&w=majority`,{
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
@@ -19,17 +18,59 @@ mongoose.connect(`mongodb+srv://admun:${process.env.DBPassword}@cluster0.vo8hxvk
     console.log(`Connected to mongoDB`);
 })
 .catch((err) => {
-    console.error(`Error conecting to Mongo: ${err}`);
-})
+    console.error(`Error connecting to Mongo: ${err}`);
+});
 
-app.use(bodyParser.json());
+
+const User = mongoose.model('User', {
+    login: String,
+    password: String
+});
+
+
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 
-app.get('/', (req,res)=>{
-    res.sendFile(path.join(__dirname,'index.html'))
+app.post('/submit', async (req, res) => {
+    try {
+        const { login, password } = req.body;
+        const saltRounds = 10; 
+        
+
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+        const user = await User.findOne({ login });
+
+        const newUser = new User({ login, password: hashedPassword });
+        await newUser.save();
+
+        res.status(200).json({ message: 'Data saved' });
+    } catch (error) {
+        console.log(`Error processing the form`);
+        res.status(500).json({ error: 'An error occurred while processing the form' });
+    }
+});
+
+app.get('/get-users', async (req, res) => {
+    try {
+        const users = await User.find({});
+        res.json(users);
+    } catch (error) {
+        console.error(`Error getting users: ${error}`);
+        res.status(500).json({ error: 'Error getting users' });
+    }
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+app.get('/home', (req,res)=>{
+    res.sendFile(path.join(__dirname,'public','home.html'))
 })
+
 
 app.listen(PORT, () => {
-    console.log(`Server work on port: ${PORT}`);
-})
+    console.log(`Server works on port: ${PORT}`);
+});
